8fce71c0c920592f0acf18c3ca73f72f627bc17b
Eliathoran <felix.klockner@e-campus.uab.cat>
2014-12-13 17:55:41 +0100

    Added js file

---
+++ Web/js/init.js
@@ -0,0 +1,30 @@
+var stage;
+
+function init() {
+	// Create state
+	stage = new createjs.Stage("kiwiCanvas");
+	// Load Sources
+	queue = new createjs.LoadQueue(true);
+	queue.installPlugin(createjs.Sound);
+	queue.addEventListener("complete", handleComplete);
+	queue.loadManifest([{id:"background", src:".\Images\background.png"}]);
+}
+
+function handleComplete(event)
+{
+	var circle = new createjs.Shape();
+	circle.graphics.beginFill("red").drawCircle(0, 0, 50);
+	circle.x = 100;
+	circle.y = 100;
+	
+	createjs.Tween.get(circle, {loop:true}).to({x:450},3000).to({x:50},3000)
+	createjs.Ticker.addEventListener("tick",tick);
+	
+	stage.addChild(circle);
+	stage.update();
+}
+
+function tick(event)
+{
+	stage.update();
+}
