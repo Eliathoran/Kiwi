bfaf69dc3ddf4fd33b99455952d647180c6da855
Eliathoran <felix.klockner@e-campus.uab.cat>
2014-12-14 18:19:29 +0100

    2 Background with speeds

--- Web/js/initGame.js
+++ Web/js/initGame.js
@@ -4,6 +4,8 @@ var backgroundFront1;
 var backgroundFront2;
 var stage;
 var kiwiAnimation;
+var backgroudSpeed;
+var backgroundFrontSpeed;
 
 function init() {
 	var canvas = document.getElementById("kiwiCanvas");
@@ -11,13 +13,9 @@ function init() {
 	
 	LoadFiles();
 	
-	kiwiAnimation.x = 20;
-	kiwiAnimation.y = 0;
-	
 	InitGame();
-	kiwiAnimation.gotoAndPlay("jump");
-	createjs.Ticker.setFPS(10);
 	
+	createjs.Ticker.setFPS(10);
 	createjs.Ticker.addEventListener("tick", tick);
 	createjs.Ticker.setInterval(25);
 	createjs.Ticker.setFPS(10);
@@ -27,46 +25,56 @@ function LoadFiles()
 {
 	// Load backgroud
 	background = new createjs.Bitmap("Images/back.jpg");
-	
-	
 	background2 = new createjs.Bitmap("Images/back.jpg");
 	
 	// Load front background
-	backgroundFront1 = new createjs.Bitmap("Images/back.jpg");
-	
-	backgroundFront2 = new createjs.Bitmap("Images/back.jpg");
+	backgroundFront1 = new createjs.Bitmap("Images/bigtree.png");
+	backgroundFront2 = new createjs.Bitmap("Images/bigtree.png");
 
 	// Load Kiwi animation
 	 var data = {
-		images: ["Images/kiwi_sprite_grande.png"],
-		frames: {width:225.5, height:188, count:12},
+		images: ["Images/kiwi_sprite.png"],
+		frames: {width:56.33, height:47, count:12},
 		animations: {
 			run:[0,10, true],
 			jump:[0,10, true],
 			die:[0,10, true]
 		}
 	};
+	// 225.5 - 188 - 12
 	
 	var ss = new createjs.SpriteSheet(data);
 	kiwiAnimation = new createjs.Sprite(ss);
 }
 
 function InitGame()
-{
+{			
+	kiwiAnimation.x = 20;
+	kiwiAnimation.y = stage.canvas.height/2;
+	kiwiAnimation.gotoAndPlay("run");
+	
+	backgroundFront1.y -= backgroundFront1.y/2;
+	backgroundFront1.y -= backgroundFront2.y/2;
 	stage.addChild(background);
 	stage.addChild(background2);
-	//stage.addChild(backgroundFront1);
-	//stage.addChild(backgroundFront2);
+	stage.addChild(backgroundFront1);
+	stage.addChild(backgroundFront2);
 	stage.addChild(kiwiAnimation);
+	
+	backgroudSpeed = -0.5;
+	backgroundFrontSpeed = -1;
 }
         
 function tick() {
-	background.x += 1;
+	
+	background.x += backgroudSpeed;
+	backgroundFront1.x += backgroundFrontSpeed;
 	
 	if (background.x >= stage.canvas.width) { background.x = 0; }
+	if (backgroundFront1.x >= stage.canvas.width) { backgroundFront1.x = 0; }
 	
 	background2.x = background.x - stage.canvas.width
-	
+	backgroundFront2.x = backgroundFront1.x - stage.canvas.width
 	
 	stage.update();
 }
