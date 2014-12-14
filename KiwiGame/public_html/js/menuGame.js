474614318c3210e0f0f253366eb313412167de22
Eliathoran <felix.klockner@e-campus.uab.cat>
2014-12-14 21:17:17 +0100

    Menu Texts

--- KiwiGame/public_html/js/menuGame.js
+++ KiwiGame/public_html/js/menuGame.js
@@ -10,27 +10,39 @@ var bestScoreMenu;
 function CreateMenu()
 {
     // Start Game Menu Item
-    startMenu = NewMenuItem();
+    startMenu = NewMenuItem("");
     startMenu.addEventListener("click", handleStartClick);
     startMenu.x = stage.canvas.width/2;
     stage.addChild(startMenu);
+    var text = new createjs.Text("Start Game", "20px Arial", "#ff7700");
+    text.x = stage.canvas.width/2+8;
+    text.y = 27;
+    stage.addChild(text);
     
     // Options Game Menu Item
-    soundMenu = NewMenuItem();
+    soundMenu = NewMenuItem("Sound Options");
     soundMenu.addEventListener("click", handleSoundClick);
     soundMenu.x = stage.canvas.width/2;
     soundMenu.y = stage.canvas.width/2.5;
     stage.addChild(soundMenu);
+    text = new createjs.Text("Sound", "20px Arial", "#ff7700");
+    text.x = stage.canvas.width/2+29;
+    text.y = 27+soundMenu.y;
+    stage.addChild(text);
     
     // Best Scores Menu Item
-    bestScoreMenu = NewMenuItem();
+    bestScoreMenu = NewMenuItem("Best Scores");
     bestScoreMenu.addEventListener("click", handleScoreClick);
     bestScoreMenu.x = stage.canvas.width/2;
     bestScoreMenu.y = stage.canvas.width/5;
     stage.addChild(bestScoreMenu);   
+    text = new createjs.Text("Best Scores", "20px Arial", "#ff7700");
+    text.x = stage.canvas.width/2+7;
+    text.y = 27+bestScoreMenu.y;
+    stage.addChild(text);
 }
 
-function NewMenuItem()
+function NewMenuItem(text)
 {
     var menu = new createjs.Shape();
     menu.graphics.beginFill("rgba(102,51,0,1)").drawRoundRect(0, 0, 120, 80, 5);
@@ -55,12 +67,16 @@ function handleStartClick(event)
 function handleSoundClick(event)
 {
     RemoveMenu();
+    // TODO: cuando haya sonido se puede elegir entre poner o quitar sonido.
+    handleStartClick(event);
     stage.update();
 }
 
 function handleScoreClick(event)
 {
     RemoveMenu();
+    // TODO: cuando haya juego se puede ver los best scores... xD
+    handleStartClick(event);
     stage.update();
 }
 
